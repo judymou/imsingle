4354e7d6d462ec6e8786662845cee4a56ab620c0
Judy Mou <moujudy@gmail.com>
2015-07-13 11:00:14 -0700

    inital commit

---
+++ app/src/main/java/com/app/imsingle/imsingle/MapsActivity.java
@@ -0,0 +1,107 @@
+package com.app.imsingle.imsingle;
+
+import android.location.Location;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity {
+
+	private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_maps);
+		setUpMapIfNeeded();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		setUpMapIfNeeded();
+	}
+
+	/**
+	 * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+	 * installed) and the map has not already been instantiated.. This will ensure that we only ever
+	 * call {@link #setUpMap()} once when {@link #mMap} is not null.
+	 * <p/>
+	 * If it isn't installed {@link SupportMapFragment} (and
+	 * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+	 * install/update the Google Play services APK on their device.
+	 * <p/>
+	 * A user can return to this FragmentActivity after following the prompt and correctly
+	 * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+	 * have been completely destroyed during this process (it is likely that it would only be
+	 * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+	 * method in {@link #onResume()} to guarantee that it will be called.
+	 */
+	private void setUpMapIfNeeded() {
+		// Do a null check to confirm that we have not already instantiated the map.
+		if (mMap == null) {
+			// Try to obtain the map from the SupportMapFragment.
+			mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+					.getMap();
+			// Check if we were successful in obtaining the map.
+			if (mMap != null) {
+				setUpMap();
+			}
+		}
+	}
+
+	/**
+	 * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+	 * just add a marker near Africa.
+	 * <p/>
+	 * This should only be called once and when we are sure that {@link #mMap} is not null.
+	 */
+	private void setUpMap() {
+		mMap.setMyLocationEnabled(true);
+			mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+				@Override
+				public void onMyLocationChange(Location location) {
+					// Create a LatLng object for the current location
+					LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+					// Show the current location in Google Map
+					mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+					// Zoom in the Google Map
+					mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+					mMap.addMarker(new MarkerOptions().position(
+							new LatLng(location.getLatitude(), location.getLongitude())).title("It's Me!"));
+				}
+			});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu items for use in the action bar
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.map_activity_actions, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle presses on the action bar items
+		switch (item.getItemId()) {
+			case R.id.action_search:
+				//refreshMap();
+				return true;
+			case R.id.action_settings:
+				//openSettings();
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
+}
